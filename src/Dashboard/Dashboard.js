cae84f83afdd0285978e37175011f0a9b3f8447f
Nihal Jadhav <nihal.jadhav@arowanaconsulting.com>
2023-01-21 22:08:13 +0530

    dashboard modified

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -8,6 +8,27 @@ function Dashboard() {
       <h2 className="text-center my-2">DASHBOARD</h2>
       <div className="container text-center">
         <hr />
+        <h5 className="text-center my-2">Assignments</h5>
+        <div className="row my-4">
+          {/* {paths.map((itm) => {
+            return ( */}
+          <div className="col-md-3">
+            <div className="card">
+              <img src="" className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">title</h5>
+                <p className="card-text">description</p>
+                <Link to="/" className="btn btn-primary">
+                  Open Project
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* );
+          })} */}
+        </div>
+        <hr />
+        <h5 className="text-center my-2">Study Projects</h5>
         <div className="row my-4">
           {paths.map((itm) => {
             return (
